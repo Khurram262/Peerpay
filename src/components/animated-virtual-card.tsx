72581c3c45cd01ec4f309746046264a67796f6a7
Muhammad Khurram <khurramimran908@gmail.com>
2025-09-17 12:00:35 +0000

    add a logo of peerpay  on virtual card on home and card with good design

--- src/components/animated-virtual-card.tsx
+++ src/components/animated-virtual-card.tsx
@@ -25,6 +25,21 @@ function MastercardLogo() {
   );
 }
 
+function PeerPayLogo() {
+    return (
+        <svg
+            role="img"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-white/90"
+            fill="currentColor"
+        >
+            <title>PeerPay</title>
+            <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm-2.88 18.514V5.486h2.16c1.359 0 2.463.315 3.312.945.85.63 1.274 1.516 1.274 2.658 0 1.05-.38 1.91-1.139 2.58-.759.67-1.78.981-3.063.981h-2.544v6.864H9.12zm2.16-8.244h2.467c.72 0 1.29-.183 1.71-.55.42-.367.63- .885.63-1.554 0-1.17-.635-1.755-1.908-1.755h-2.899v3.859z" />
+        </svg>
+    )
+}
+
 const formatCardNumber = (cardNumber: string) => {
     return cardNumber.replace(/(\d{4})/g, '$1 ').trim();
 };
@@ -74,6 +89,7 @@ export function AnimatedVirtualCard({ card, isVisible = false }: { card: Virtual
 
             <div className="flex items-center gap-2">
               <Wifi className="h-5 w-5 sm:h-6 sm:w-6 text-white/80 rotate-90" />
+              <PeerPayLogo />
             </div>
           </div>
 
